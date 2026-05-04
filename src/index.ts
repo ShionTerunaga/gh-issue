8f7faaeb8f204548b1fcec3dc367111d43708560
ShionTerunaga <sion2769@gmail.com>
2026-05-04 18:40:23 +0900

    fix: fmt

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,6 @@
 import { run } from "./run";
 
-export {
-  createIssueTemplate,
-  createIssueTemplateYaml,
-  initIssueTemplates,
-} from "./templates";
+export { createIssueTemplate, createIssueTemplateYaml, initIssueTemplates } from "./templates";
 
 export async function main(argv = process.argv) {
   await run(argv);
